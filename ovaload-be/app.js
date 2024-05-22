87436da9f3110b31a81334098655cbcc2c9c2d74
rmachaal <rahafam@hotmail.co.uk>
2024-05-22 09:34:26 +0100

    Updates getExerciseById endpoint

--- ovaload-be/app.js
+++ ovaload-be/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const {getExercises} = require("./controllers/getExercises")
+const { getExercises } = require("./controllers/getExercises")
+const { getExerciseById } = require("./controllers/getExerciseById")
 const {getExercisesByDate} = require("./controllers/getExercisesByDate")
 const {postPlannedExercise} = require("./controllers/postPlannedExercise")  
 const Model = require("./models/user")
@@ -9,8 +10,8 @@ const app = express()
 app.use(express.json())
 
 app.get("/api/:user/exercises", getExercises)
+app.get("/api/:user/:exercise", getExerciseById);
 app.get("/api/:user/exercises/:date",getExercisesByDate)
-
 app.post("/api/:user/plannedExercises",postPlannedExercise)
 
 module.exports = app
