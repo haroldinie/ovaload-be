271d1bc9709445b7179b84ea184b90ad2f0bca71
haroldinie <haroldinie123@gmail.com>
2024-05-24 10:01:54 +0100

    allow cross origin resource sharing

--- ovaload-be/app.js
+++ ovaload-be/app.js
@@ -17,11 +17,15 @@ const {
   getAllScores
 } = require("./controllers/getLeaderboardScore");
 const { patchNewFriendByUsername } = require("./controllers/patchNewFriendByUsername")
+const cors = require('cors');
 
 require("dotenv").config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+
 
 app.get("/api/:user/exercises", getExercises);
 app.post("/api/:user/exercises", postExercise);
