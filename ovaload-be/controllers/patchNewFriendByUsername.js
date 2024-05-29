e96118d2d3985b79ff0e59854d3204cb10736569
rmachaal <rahafam@hotmail.co.uk>
2024-05-29 11:08:40 +0100

    Updates patch new friend endpoint.

--- ovaload-be/controllers/patchNewFriendByUsername.js
+++ ovaload-be/controllers/patchNewFriendByUsername.js
@@ -1,20 +1,25 @@
-const User = require("../models/user")
+const User = require("../models/user");
 
 exports.patchNewFriendByUsername = async (req, res) => {
-    const username = req.body.username
-    const loggedInUser = req.params.user
+  const friend = req.body.friend;
+  const loggedInUser = req.params.user;
 
-    try {
-        const newFriend = await User.where("username").equals(username)
-        if (!newFriend) {
-            return res.status(404).json({ message: "User not found" })
-        } 
-       const updatedLoggedInUser = await User.findOneAndUpdate({username: loggedInUser}, {$push: {friends: username}},  { new: true, useFindAndModify: false })
-       await updatedLoggedInUser.save()
-       res.status(200).json({updatedFriendsList: updatedLoggedInUser.friends})
+  try {
+    const newFriend = await User.where("username").equals(friend);
+    if (!newFriend) {
+      return res.status(404).json({ message: "User not found" });
     }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "An error occurred while finding the user" });
-      }
-}
+    const updatedLoggedInUser = await User.findOneAndUpdate(
+      { username: loggedInUser },
+      { $push: { friends: friend } },
+      { new: true, useFindAndModify: false }
+    );
+    await updatedLoggedInUser.save();
+    res.status(200).json({ updatedFriendsList: updatedLoggedInUser.friends });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "An error occurred while finding the user" });
+  }
+};
